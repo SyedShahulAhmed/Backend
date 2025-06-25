e4050eff699305997e0328127ff8f0c3cc4dfb71
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-06-26 00:14:27 +0530

    Checking  HealthCheck

--- PlayTube/src/app.js
+++ PlayTube/src/app.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express()
+const app = express();
 
 app.use(
-    cors({
-        origin : process.env.CORS_ORIGIN,
-        credentials : true,
-    })
-)
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-//common middleware
+// Common middlewares
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
 
-app.use(express.json({limit : "16kb"})) //JSON LIMIT
+// Import routes
+import healthCheckRouter from './routes/HealthCheck.routes.js';
 
-app.use(express.urlencoded({extended : true, limit : "16Kb"}))
+// Use routes
+app.use('/api/v1/healthCheck', healthCheckRouter);
 
-app.use(express.static("public")) 
-
-
-export { app }
+export { app };
