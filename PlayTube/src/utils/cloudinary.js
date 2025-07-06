f05559c1d42d81ce3329e7d11be51a33895ebb1f
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-07-06 09:18:26 +0530

    Cloudinary upload func created

---
+++ PlayTube/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        const response =  await cloudinary.uploader.upload(
+            localFilePath, {
+                resource_type : "auto",
+            }
+        )
+        console.log("File Uploaded on cloudinary> file src : " + response.url)
+        //once file is Uploaded , we would like to delete it from our server
+        fs.unlinkSync(localFilePath)
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+ }
+ export {uploadOnCloudinary}
