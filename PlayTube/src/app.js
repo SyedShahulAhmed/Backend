405bbdd7a1fd6394b2fa250ccd3565ff9993e28c
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-07-10 23:32:18 +0530

    Added error handler

--- PlayTube/src/app.js
+++ PlayTube/src/app.js
@@ -14,11 +14,15 @@ app.use(
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
+app.use(cookieparser())
 
 // Import routes
 import healthCheckRouter from './routes/HealthCheck.routes.js';
+import userRouter from './routes/User.routes.js'
+import { errorhandler } from './middlewares/error.middlewares.js';
 
 // Use routes
 app.use('/api/v1/healthCheck', healthCheckRouter);
-
+app.use('/api/v1/users', userRouter);
+app.use(errorhandler)
 export { app };
