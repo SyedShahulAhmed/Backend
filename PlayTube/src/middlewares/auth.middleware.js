d32a73cf797a891025789e3e7f75d2b75e15eb62
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-07-22 23:49:08 +0530

    Authentication middleware

--- PlayTube/src/middlewares/auth.middleware.js
+++ PlayTube/src/middlewares/auth.middleware.js
@@ -1,20 +1,27 @@
 import jwt from "jsonwebtoken";
-import {User} from "../models/user.model"
-import {ApiError} from "../utils/ApiError"
-import {asynchandler} from "../utils/asyncHandler"
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asynchandler } from "../utils/asyncHandler.js";
 
-export const verifyJWT = asynchandler(async(req,_,next) =>{
-    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer","")
+export const verifyJWT = asynchandler(async (req, _, next) => {
+  const token =
+    req.cookies.accessToken ||
+    req.header("Authorization")?.replace("Bearer", "");
 
-    if(!token){
-        throw new ApiError(404,"UnAuthorized")
+  if (!token) {
+    throw new ApiError(404, "UnAuthorized");
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if(!user){
+        throw new ApiError(400,"User Not found")
     }
-    try {
-        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-        await User.findById(decodedToken?._id).select("-password -refreshToken")
-    req.user = user
-    next()
-    } catch (error) {
-        throw new ApiError(401,error?.message || "Invalid access token")
-    }
-})
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
